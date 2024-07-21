7c811e4a82b837d81127ac1f02c0af97d072ebf8
asafegithubs <g.a.oliveirastudios@gmail.com>
2024-07-21 14:19:09 -0300

    commit

---
+++ script.js
@@ -0,0 +1,37 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const imageContainer = document.querySelector('.use');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+
+    observer.observe(imageContainer);
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const imageContainer = document.querySelector('.imagem-eu');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+
+    observer.observe(imageContainer);
+}); 
+
