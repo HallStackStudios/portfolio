7965a94311e52d7e77f8a5efe6f6e8b8b18de731
asafegithubs <g.a.oliveirastudios@gmail.com>
2024-07-31 20:18:31 -0300

    commit

--- script.js
+++ script.js
@@ -20,17 +20,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
     observer.observe(imageContainer);
 });
-document.addEventListener('DOMContentLoaded', function() {
-    const imageContainer = document.querySelector('.imagem-eu');
-
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-                observer.unobserve(entry.target);
-            }
-        });
-    }, { threshold: 0.1 });
-
-    observer.observe(imageContainer);
-}); 
