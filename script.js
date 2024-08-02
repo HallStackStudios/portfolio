aec9a62dd5be17f7ca16f87626974fa5d73f0c1f
asafegithubs <g.a.oliveirastudios@gmail.com>
2024-08-02 12:43:04 -0300

    commit

--- script.js
+++ script.js
@@ -61,3 +61,28 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(image);
     });
 });
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const topBar = document.querySelector('#top-bar');
+
+    const options = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible2');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    // Observar a top-bar para a animação
+    observer.observe(topBar);
+
+    // Observar imagens para a animação
+    images.forEach(image => {
+        observer.observe(image);
+    });
+});
